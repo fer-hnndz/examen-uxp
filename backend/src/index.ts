7985547a8e3c07e9f6e4bc1940b8ecfb00ac16f2
fer-hnndz <65425622+fer-hnndz@users.noreply.github.com>
2024-12-11 11:15:20 -0600

    detalles

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,94 +1,38 @@
 import sequelize, { Restaurant, RestaurantAvailability } from "./database";
-import express, { Request, Response } from "express";
+import { Op } from "sequelize";
+import express, { Request, Response, } from "express";
 import cors from "cors";
+import dayjs from "dayjs";
 
 const app = express();
 app.use(cors());
 
-app.get("/", async (req, res) => {
-    Restaurant.create({
-        name: "La Casca",
-        description: "Consumo en el lugar · Terraza o mesas al aire libre · Retiro desde el coche"
-    })
-
-    Restaurant.create({
-        name: "El Portal de las Carnes",
-        description: "Disfrute de las mejores carnes y vinos en un ambiente único de la ciudad de San Pedro Sula.\n"
-    })
-
-    Restaurant.create({
-        name: 'Factory Steak & Lobster',
-        description: 'Experience the best steak and lobster in San Pedro Sula at Factory Steak and Lobster'
-    })
-
-
-    RestaurantAvailability.create({
-        restaurant_id: 1,
-        schedule_time: "2023-06-20 08:00:00",
-        reserved: false,
-        reserved_by: null
-    })
-
-    RestaurantAvailability.create({
-        restaurant_id: 1,
-        schedule_time: "2023-06-20 09:00:00",
-        reserved: false,
-        reserved_by: null
-    })
-
-    RestaurantAvailability.create({
-        restaurant_id: 2,
-        schedule_time: "2023-06-21 10:00:00",
-        reserved: false,
-        reserved_by: null
-    })
-
-    RestaurantAvailability.create({
-        restaurant_id: 2,
-        schedule_time: "2023-06-21 11:00:00",
-        reserved: false,
-        reserved_by: null
-    })
-
-    RestaurantAvailability.create({
-        restaurant_id: 3,
-        schedule_time: "2023-06-22 07:00:00",
-        reserved: false,
-        reserved_by: null
-    })
-
-
-    RestaurantAvailability.create({
-        restaurant_id: 3,
-        schedule_time: "2023-06-22 09:00:00",
-        reserved: false,
-        reserved_by: null
-    })
-
-    RestaurantAvailability.create({
-        restaurant_id: 1,
-        schedule_time: "2021-06-20 08:00:00",
-        reserved: false,
-        reserved_by: null
-    })
-
-    return res.status(200).json({ message: "Hello World" });
-});
-
 app.get("/restaurants", async (req, res) => {
     let { limit, offset } = req.query;
 
-    const restaurants = await Restaurant.findAll();
+    let restaurants = await Restaurant.findAll();
 
 
     limit = (limit) ? limit : String(restaurants.length);
     offset = (offset) ? offset : String(0);
 
-    restaurants.slice(Number(offset), Number(offset) + Number(limit));
-
     return res.status(200).json(restaurants);
 
 });
+
+app.get("/restaurants/availability", async (req, res) => {
+    const today = dayjs();
+
+    const l = RestaurantAvailability.findAll({
+        schedule_time: {
+            [Op.gte]: today.toDate()
+        }
+    })
+
+
+    return res.status(200).json(l);
+
+})
 
 app.listen(3000, () => {
     sequelize.sync();
